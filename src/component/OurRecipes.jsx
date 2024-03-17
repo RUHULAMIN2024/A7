cbc02e4848de22e8a3699e5157daf57994829752
RUHULAMIN2024 <ruhulamin15jun1999@gmail.com>
2024-03-17 11:55:46 +0600

    final_project

--- src/component/OurRecipes.jsx
+++ src/component/OurRecipes.jsx
@@ -2,7 +2,7 @@
 const OurRecipes = () => {
     return (
         <div className="mt-20 mb-12 text-center">
-            <h2 className="mb-2 text-4xl font-bold">Our Recipes</h2>
+            <h2 className="mb-3 text-4xl font-bold">Our Recipes</h2>
             <p>Cook spaghetti al dente. Fry pancetta until crispy. Mix eggs, Parmesan, and pepper. Toss spaghetti <br/> with egg mixture. Serve hot and garnish generously. Enjoy!</p>
         </div>
     );
